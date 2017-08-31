640f23924f6125d299a92f61ae291b9c1e1c4082
日比野佑亮 <hibino616@gmail.com>
2017-08-31 16:37:35 +0900

    practice string
    文字列操作練習

---
+++ string.py
@@ -0,0 +1,21 @@
+#coding: UTF-8
+
+a = "Hello"
+b = "World!"
+
+c = a + b
+print c
+
+d = a * 5
+print d
+
+e = a * 5 + b * 3
+print e
+
+# すべて大文字
+f = a.upper()
+print f
+
+# すべて小文字
+g = a.lower()
+print g
