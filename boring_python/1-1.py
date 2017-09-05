ab9a3fc8855cac1583d5033785242447ca335559
日比野佑亮 <hibino616@gmail.com>
2017-09-05 16:33:04 +0900

    add python trraining

---
+++ boring_python/1-1.py
@@ -0,0 +1,21 @@
+
+a = 2 + 2
+print(a)
+
+b = "Hello World!"
+print(b)
+
+c = "Hello " + "World!"
+print(c)
+
+d = "Hello" * 5
+print(d)
+
+# print('What your name?')
+# my_name = input()
+# print(my_name)
+# コメント
+
+
+
+
