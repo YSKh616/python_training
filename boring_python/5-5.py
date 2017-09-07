03bd8aab11268afc752e6f4ef72cfb434f9391a6
日比野佑亮 <hibino616@gmail.com>
2017-09-08 02:25:37 +0900

    practice dictionary method
    setdefault() method

--- boring_python/5-5.py
+++ boring_python/5-5.py
@@ -3,3 +3,6 @@ num = 20
 picnic_items = {'apples': 5, 'cups': 2}
 print('I am bringing ' + str(picnic_items.get('eggs', num)) + ' eggs.')
 
+spam = {'name': 'Pooka', 'age': 5}
+spam.setdefault('color', 'black')
+print(spam)
