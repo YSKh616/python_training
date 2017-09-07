ef15186f8013e367ad67fd03a9e0786e3f7e4a0c
日比野佑亮 <hibino616@gmail.com>
2017-09-07 23:32:13 +0900

    edited 5-4.py
    print func

--- boring_python/5-4.py
+++ boring_python/5-4.py
@@ -1,6 +1,6 @@
-the_board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
-             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
-             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+the_board = {'top-L': 'O', 'top-M': 'O', 'top-R': 'O',
+             'mid-L': 'X', 'mid-M': 'X', 'mid-R': ' ',
+             'low-L': ' ', 'low-M': ' ', 'low-R': 'X'}
 
 def print_board(board):
     print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
