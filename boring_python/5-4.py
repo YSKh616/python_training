631c2d9cdb6b3c2d6c73d4a353d7027a3687cf02
日比野佑亮 <hibino616@gmail.com>
2017-09-07 23:34:29 +0900

    edited 5-4.py
    add user input

--- boring_python/5-4.py
+++ boring_python/5-4.py
@@ -1,6 +1,6 @@
-the_board = {'top-L': 'O', 'top-M': 'O', 'top-R': 'O',
-             'mid-L': 'X', 'mid-M': 'X', 'mid-R': ' ',
-             'low-L': ' ', 'low-M': ' ', 'low-R': 'X'}
+the_board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 
 def print_board(board):
     print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
@@ -8,5 +8,16 @@ def print_board(board):
     print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
     print('-+-+-')
     print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+turn = 'X'
+for i in range(9):
+    print_board(the_board) # ❶
+    print(turn + 'の番です。どこに打つ？')
+    move = input()         # ❷
+    the_board[move] = turn # ❸
+    if turn == 'X':        # ❹
+        turn = 'O'
+    else:
+        turn = 'X'
 
 print_board(the_board)
