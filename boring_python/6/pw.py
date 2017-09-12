d36047d7565b24900b24e3397ec2cdc8b8ce41ba
日比野佑亮 <hibino616@gmail.com>
2017-09-12 23:48:45 +0900

    practice command line argv

---
+++ boring_python/6/pw.py
@@ -0,0 +1,14 @@
+#! python3
+# パスワード管理プログラム
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+             'luggage': '12345'}
+
+import sys
+if len(sys.argv) < 2:
+    print('使い方: python pw.py [アカウント名]')
+    print('パスワードをクリップボードにコピーします')
+    sys.exit()
+
+account = sys.argv[1]   # 最初のコマンドラインの引数がアカウント名
+
