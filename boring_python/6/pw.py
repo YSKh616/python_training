e6fdd502fd438a779d60ae55264a79e4c3e2661d
日比野佑亮 <hibino616@gmail.com>
2017-09-12 23:57:04 +0900

    make password copy program

--- boring_python/6/pw.py
+++ boring_python/6/pw.py
@@ -5,10 +5,18 @@ PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
              'luggage': '12345'}
 
 import sys
+import pyperclip
+
 if len(sys.argv) < 2:
     print('使い方: python pw.py [アカウント名]')
     print('パスワードをクリップボードにコピーします')
     sys.exit()
 
 account = sys.argv[1]   # 最初のコマンドラインの引数がアカウント名
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print(account + 'のパスワードをクリップボードにコピーしました')
+else:
+    print(account + 'というアカウントはありません')
 
