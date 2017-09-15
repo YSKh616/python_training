9035aa0c74ee9d51d15ad3dd74178ec73a2c548a
日比野佑亮 <hibino616@gmail.com>
2017-09-15 16:10:48 +0900

    edit match  pattern

--- boring_python/7/mo.py
+++ boring_python/7/mo.py
@@ -1,6 +1,6 @@
 import re
 
-phone_num_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phone_num_regex = re.compile(r'\d{3}-\d{3}-\d{4}')
 mo = phone_num_regex.search('私の電話番号は415-555-4242です。')
 print('電話番号が見つかりました: ' + mo.group())
 
