7654da5ff79a5a368831038f6a4f04898ff0785c
日比野佑亮 <hibino616@gmail.com>
2017-09-15 16:21:42 +0900

    practice group, ( ) use

--- boring_python/7/mo.py
+++ boring_python/7/mo.py
@@ -1,6 +1,7 @@
 import re
 
-phone_num_regex = re.compile(r'\d{3}-\d{3}-\d{4}')
+phone_num_regex = re.compile(r'(\d{3})-(\d{3}-\d{4})')
 mo = phone_num_regex.search('私の電話番号は415-555-4242です。')
 print('電話番号が見つかりました: ' + mo.group())
-
+print(mo.group(1))
+print(mo.group(2))
