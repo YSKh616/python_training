8e48edffcce0593bb83e8178c67745d499feafaa
日比野佑亮 <hibino616@gmail.com>
2017-09-15 16:24:59 +0900

    practice groups method

--- boring_python/7/mo.py
+++ boring_python/7/mo.py
@@ -5,3 +5,7 @@ mo = phone_num_regex.search('私の電話番号は415-555-4242です。')
 print('電話番号が見つかりました: ' + mo.group())
 print(mo.group(1))
 print(mo.group(2))
+area_code, main_number = mo.groups()
+print(area_code)
+print(main_number)
+
